328febc48f5985f6abd864a87eff263df9f3ca23
lw-92 <670415962@qq.com>
2018-09-17 21:02:31 +0800

    first commit

---
+++ src/main/java/com/lee/li/lee/LeeApplication.java
@@ -0,0 +1,12 @@
+package com.lee.li.lee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeeApplication.class, args);
+	}
+}
